e577e43400c331c015a146cbdcf01d016cbf0bed
1ldari <93144578+1ldari@users.noreply.github.com>
2022-03-29 09:42:04 +0300

    Update Task43.sql

--- Task43.sql
+++ Task43.sql
@@ -1,10 +1,18 @@
 --3.	Показать категорию (ProductSubcategoryID) товаров, для которой существует более 7 размеров (Таблица Production.Product). Показать поля [Name], Size и ProductSubcategoryID.
-use AdventureWorks2019
-
-select Name, ProductSubcategoryID from Production.Product
-where ProductSubcategoryID in
-(select ID from
-(select ProductSubcategoryID as ID, count(Size) as cnt
-from Production.Product
-group by ProductSubcategoryID) A
-where cnt>7)
+SELECT COUNT(DISTINCT p2.Size) AS SizeCount 
+,p2.ProductSubcategoryID 
+FROM Production.Product AS p2 
+WHERE p2.ProductSubcategoryID IS NOT NULL 
+AND p2.Size IS NOT NULL 
+GROUP BY p2.ProductSubcategoryID 
+HAVING COUNT(DISTINCT p2.Size) > 7;
+SELECT p1.[Name] 
+,p1.ProductSubcategoryID
+,p1.Size FROM Production.Product AS p1
+WHERE p1.ProductSubcategoryID IS NOT NULL 
+AND p1.Size IS NOT NULL 
+AND ( SELECT COUNT(DISTINCT p2.Size) AS SizeCount 
+FROM Production.Product AS p2 
+WHERE p1.ProductSubcategoryID = p2.ProductSubcategoryID
+AND p2.Size IS NOT NULL 
+) > 7;
