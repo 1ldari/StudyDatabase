7955ce1d9c5a56c292ca1a9cbb3f6246af6659e9
1ldari <93144578+1ldari@users.noreply.github.com>
2022-03-29 08:43:20 +0300

    Update Task25.sql

--- Task25.sql
+++ Task25.sql
@@ -1,10 +1,7 @@
 -- Вывести ProductID из таблицы Production.ProductListPriceHistory у которых средняя цена (ListPrice) меньше 10. Добавить столбец с рангом, определяя его порядок в зависимости от количества строк, содержащих среднюю цену. (Использовать RANK)
-
-USE  AdventureWorks2019
-
-select ProductId, cnt, rank() over(order by cnt) rnk
-from
-(select ProductId, avg(ListPrice) as avg_price, count(ListPrice) as cnt
-from Production.ProductListPriceHistory
-group by ProductId) A
-where avg_price < 10
+USE AdventureWorks2019
+SELECT RANK() OVER(ORDER BY (ListPrice)) AS N 
+, ProductID 
+, (ListPrice) 
+FROM Production.ProductListPriceHistory
+Where (ListPrice) <10;
