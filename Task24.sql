83180a813fb1934a5835f0aafe78db76d0a3cb31
1ldari <93144578+1ldari@users.noreply.github.com>
2022-03-29 08:43:00 +0300

    Update Task24.sql

--- Task24.sql
+++ Task24.sql
@@ -1,5 +1,5 @@
---Найти цвета из таблицы Production.Product, где количество различных размеров больше 0 (из рассмотрения исключить нулевые значения цвета).--
+4) --Найти цвета из таблицы Production.Product, где количество различных размеров больше 0 (из рассмотрения исключить нулевые значения цвета).--
+USE AdventureWorks2019
 SELECT  Color
 FROM Production.Product
-WHERE Size != 'NULL'
-
+WHERE Size IS NOT NULL
