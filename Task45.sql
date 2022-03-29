4b2b0739548b3e44f76de0bf7e0e0aea007387b5
1ldari <93144578+1ldari@users.noreply.github.com>
2022-03-29 09:28:56 +0300

    Update Task45.sql

--- Task45.sql
+++ Task45.sql
@@ -1,9 +1,9 @@
 --5.	Показать товары, цена которых больше средней цены в любом размере (Таблица Production.Product). Показать поля [Name], Size и ListPrice.
- use AdventureWorks2019
+use AdventureWorks2019
 
 select Name, Size, ListPrice 
 from Production.Product A
-where ListPrice>
+where ListPrice > ANY
 (select avg_price from
 (select size, avg(ListPrice) as avg_price
 from Production.Product
