c7a0172aa78d5790dde04e4c4b96ee784c206a5b
1ldari <93144578+1ldari@users.noreply.github.com>
2022-03-29 09:09:15 +0300

    Update Task34.sql

--- Task34.sql
+++ Task34.sql
@@ -1,7 +1,8 @@
 -- Показать список покупателей (CustomerID), к которым относятся несколько сроков выполнения заказа (DueDate), из таблицы Sales.SalesOrderHeader, используя SELF JOIN.
-
 use AdventureWorks2019
 
-select CustomerID, Count(*) 
-from Sales.SalesOrderHeader
-group by CustomerID
+SELECT DISTINCT v1.*
+FROM Sales.SalesOrderHeader AS v1 
+INNER JOIN Sales.SalesOrderHeader AS v2
+ON v1.DueDate<>  v2.DueDate
+WHERE v1.CustomerID = v2.CustomerID
