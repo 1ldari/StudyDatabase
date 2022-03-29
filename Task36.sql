c4623ebbeb50664e6909007e5840869f40fd4754
1ldari <93144578+1ldari@users.noreply.github.com>
2022-03-29 12:33:59 +0300

    Update Task36.sql

--- Task36.sql
+++ Task36.sql
@@ -1,8 +1,11 @@
 -- 6.	Показать подкатегории продуктов (ProductSubcategoryID из таблицы Production.Product), которые имеют несколько цветов, содержащих в названии букву “e”, используя SELF JOIN.
 
 use AdventureWorks2019
-
-select ProductSubcategoryID, count(Color) 
-from Production.Product
-where color like '%e%'
-group by ProductSubcategoryID
+select DISTINCT V1.* 
+from Production.Product AS V1 
+INNER JOIN Production.Product AS V2
+ON V1.ProductSubcategoryID = V2.ProductSubcategoryID
+where V1.Color <> V2.Color
+SELECT count(Color) from Production.Product
+where color LIKE  '%e%' 
+group by ProductSubcategoryID 
