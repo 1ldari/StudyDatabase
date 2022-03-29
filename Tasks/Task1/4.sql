9b87e49dfb9b5d850d5d60834c23bb16389297d5
1ldari <93144578+1ldari@users.noreply.github.com>
2022-03-29 08:41:27 +0300

    Update 4.sql

--- Tasks/Task1/4.sql
+++ Tasks/Task1/4.sql
@@ -1,4 +1,5 @@
 --4.	Из таблицы Purchasing.ProductVendor показать ID товара (ProductID), минимальное и максимальное количество
 --заказов (MinOrderQty и MaxOrderQty) и существующее количество заказов на данный момент (OnOrderQty).
-SELECT ProductID, MinOrderQty, MaxOrderQty FROM Purchasing.ProductVendor
+use AdventureWorks2019
+SELECT ProductID, MinOrderQty, MaxOrderQty,OnOrderQty  FROM Purchasing.ProductVendor
 WHERE OnOrderQty IS NOT NULL
